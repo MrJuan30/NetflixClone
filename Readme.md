8847bee2ee50ae7fb24719c2d38cb699e8b35bd9
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-20 21:36:08 -0500

    basic config

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Link de la API:
+
+Utilice para crear CSS sin tailwind
+
+@layer base {
+h1 {
+font-size: 200px;
+}
+}
+
+que mas pongo?
+
+si
+
+saludos
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  colors: {
+    "primary-red": "#e50914",
+    "primary-black": "#141010",
+    "primary-white": "#f8f3e6",
+    "primary-gray": "#232323",
+    "seconday-red": "#b81d24",
+  },
+  plugins: [],
+};
