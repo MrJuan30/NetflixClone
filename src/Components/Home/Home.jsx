41975e98659221e01fa67fe65f02cc5cd76f39fe
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-26 23:06:33 -0500

    finished big card

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import requests from '../Request'
 const Home = () => {
     const [Movies, setMovies] = useState([])
-    const movie = Movies[Math.floor(Math.random() * Movies.length - 1)]
+    const movie = Movies[Math.floor(Math.random() * Movies.length)]
 
     useEffect(() => {
         axios
@@ -14,14 +14,32 @@ const Home = () => {
             .catch((err) => console.log(err))
     }, [])
 
-    console.log(movie)
+    // console.log(movie)
+
+    const truncateString = (str, num) => {
+        if (str?.length > num) {
+            return str.slice(0, num) + '...'
+        } else {
+            return str;
+        }
+    }
 
 
     return (
-        <div className='w-full h-[550px] text-primary-white'>
+        <div className='w-full h-[700px] text-primary-white'>
             <div className='w-full h-full'>
-                <div className='absolute w-full h-[550px] bg-gradient-to-r from-primary-black'></div>
-                <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+                <div className='absolute w-full h-[700px] bg-gradient-to-r from-primary-black'></div>
+                <img className='w-full h-full object-cover bg-center content-center' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+
+                <div className='absolute w-full top-[20%] p-4 md:p-8 gap-4'>
+                    <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+                    <div className='my-9'>
+                        <button className='border bg-primary-white text-primary-black border-primary-white py-2 px-5'>Play</button>
+                        <button className='border text-primary-white border-primary-white py-2 px-5 ml-4'>Watch Later</button>
+                    </div>
+                    <p className='text-primary-white text-sm'>Released: {movie?.release_date}</p>
+                    <p className='w-full md:max-w-[70&] lg:max-w-[50%] xl:max-w-[35%] text-primary-white'>{truncateString(movie?.overview, 150)}</p>
+                </div>
             </div>
         </div>
     )
