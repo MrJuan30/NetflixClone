e9e1c00cc24cd7f2cad96b98adb6c7f206916af2
saac17 <saac17062003@gmail.com>
2023-01-26 22:12:54 -0500

    navbar design

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const NavBar = () => {
     return (
         <>
-
+            <div className='Navbar_Container flex items-center justify-between p-4 z-[100] w-full absolute'>
+                <h1 className='text-primary-red font-bebas text-4xl cursor-pointer'>NETCLONE</h1>
+                <div>
+                    <button className='text-primary-white pr-4'>Sing In</button>
+                    <button className='bg-primary-red font-raleway px-6 py-2 rounded cursor-pointer text-primary-white'>Sing Up</button>
+                </div>
+            </div>
         </>
     )
 }
