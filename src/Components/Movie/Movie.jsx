a3af9a7506b10c9fa0f20e58d58c5a71304574a4
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-28 16:04:39 -0500

    minimum changes

--- src/Components/Movie/Movie.jsx
+++ src/Components/Movie/Movie.jsx
@@ -7,7 +7,7 @@ const Movie = ({ item }) => {
         <>
             <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
                 <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                <div className='absolute flex items-center justify-center top-0 left-0 w-full h-full hover:bg-primary-black/50 opacity-0 hover:opacity-100 text-primary-white'>
+                <div className='absolute flex items-center justify-center top-0 left-0 w-full h-full hover:bg-[#000]/50 opacity-0 hover:opacity-100 text-primary-white'>
                     <p className='white-space-normal text-sm md:text-sm font-bold capitalize'>{item?.title}</p>
                     <p>
                         {Like ? <FaRegHeart className='absolute top-4 left-4 text-primary-white text-xl' /> : <FaHeart className='absolute top-4 left-4 text-primary-white text-xl' />}
