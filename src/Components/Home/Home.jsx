57a027f0729c70390f6a9440d8fbbd754086be5d
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-27 22:00:45 -0500

    minimum changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -29,10 +29,10 @@ const Home = () => {
         <div className='w-full h-[550px] content-center text-primary-white xl:h-[850px]'>
             <div className='w-full h-full'>
                 <div className='absolute w-full h-[550px] bg-gradient-to-r from-primary-black xl:h-[850px]' ></div>
-                <img className='w-full h-full object-cover aspect-video ' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+                <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
                 <div className='absolute w-full top-[20%] p-4 md:p-8 gap-4'>
                     <h1 className='text-3xl md:text-5xl font-bold xl:text-6xl'>{movie?.title}</h1>
-                    <div className='my-9'>
+                    <div className='my-12'>
                         <button className='border bg-primary-white text-primary-black border-primary-white py-2 px-5'>Play</button>
                         <button className='border text-primary-white border-primary-white py-2 px-5 ml-4'>Watch Later</button>
                     </div>
