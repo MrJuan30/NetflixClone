d333cd1ae8ff33d0ff46357e11c18d0a4cdf51db
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-28 15:14:52 -0500

    fixed some scroll problems

---
+++ src/Components/Movie/Movie.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { FaRegHeart, FaHeart } from 'react-icons/fa'
+
+const Movie = ({ item }) => {
+    const [Like, setLike] = useState(true)
+    return (
+        <>
+            <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+                <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
+                <div className='absolute flex items-center justify-center top-0 left-0 w-full h-full hover:bg-primary-black/50 opacity-0 hover:opacity-100 text-primary-white'>
+                    <p className='white-space-normal text-sm md:text-sm font-bold capitalize'>{item?.title}</p>
+                    <p>
+                        {Like ? <FaRegHeart className='absolute top-4 left-4 text-primary-white text-xl' /> : <FaHeart className='absolute top-4 left-4 text-primary-white text-xl' />}
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Movie
--- src/Components/Rows/Row.jsx
+++ src/Components/Rows/Row.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { FaRegHeart, FaHeart } from 'react-icons/fa'
+import Movie from '../Movie/Movie';
 
 const Row = ({ title, fetchURL }) => {
 
     const [Movies, setMovies] = useState([]);
-    const [Like, setLike] = useState(true)
+
 
     useEffect(() => {
         axios.get(fetchURL)
@@ -23,17 +23,9 @@ const Row = ({ title, fetchURL }) => {
         <>
             <h2 className='text-primary-white font-bold md:text-xl p-4'>{title}</h2>
             <div className='relative flex items-center'>
-                <div id={'slider'}>
+                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
                     {Movies.map((item, id) => (
-                        <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                            <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                            <div className='absolute flex items-center justify-center top-0 left-0 w-full h-full hover:bg-primary-black/60 opacity-0 hover:opacity-100 text-primary-white'>
-                                <p className='white-space-normal text-sm md:text-sm font-bold capitalize'>{item?.title}</p>
-                                <p>
-                                    {Like ? <FaRegHeart className='absolute top-4 left-4 text-primary-white text-xl' /> : <FaHeart className='absolute top-4 left-4 text-primary-white text-xl' />}
-                                </p>
-                            </div>
-                        </div>
+                        <Movie item={item} key={id} />
                     ))}
                 </div>
             </div>
