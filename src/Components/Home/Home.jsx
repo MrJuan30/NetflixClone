012e6d8a0cc3d1d4003ac2f673d128325b94f4d8
Juan Cantor <sebastiancantorvc@gmail.com>
2023-01-28 11:59:39 -0500

    minimum changes to the responsive design

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -26,19 +26,19 @@ const Home = () => {
 
 
     return (
-        <div className='w-full h-[550px] content-center text-primary-white xl:h-[850px]'>
+        <div className='w-full h-[550px] content-center text-primary-white 2xl:h-[1000px]'>
             <div className='w-full h-full'>
-                <div className='absolute w-full h-[550px] bg-gradient-to-r from-primary-black xl:h-[850px]' ></div>
+                <div className='absolute w-full h-[550px] bg-gradient-to-r from-primary-black 2xl:h-[1000px]'></div>
                 <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
-                <div className='absolute w-full top-[20%] p-4 md:p-8 gap-4'>
-                    <h1 className='text-3xl md:text-5xl font-bold xl:text-6xl'>{movie?.title}</h1>
+                <div className='absolute w-full top-[20%] p-4 md:p-8 gap-4  lg:top-[10%] xl:top-[20%]'>
+                    <h1 className='text-2xl md:text-4xl font-semibold  2xl:text-7xl'>{movie?.title}</h1>
                     <div className='my-12'>
-                        <button className='border bg-primary-white text-primary-black border-primary-white py-2 px-5'>Play</button>
-                        <button className='border text-primary-white border-primary-white py-2 px-5 ml-4'>Watch Later</button>
+                        <button className='border bg-primary-white text-primary-black border-primary-white py-2 px-5 2xl: lg:text-2xl'>Play</button>
+                        <button className='border text-primary-white border-primary-white py-2 px-5 ml-4 lg:text-2xl'>Watch Later</button>
                     </div>
-                    <p className='text-primary-white text-sm xl:text-xl'>Released: {movie?.release_date}</p>
+                    <p className='text-primary-white font-extralight text-sm xl:text-xl 2xl:text-3xl'>Released: {movie?.release_date}</p>
                     <br />
-                    <p className=' font-thin w-full md:max-w-[70&] lg:max-w-[50%] xl:max-w-[35%] xl:text-xl text-primary-white'>{truncateString(movie?.overview, 150)}</p>
+                    <p className='font-light w-full md:max-w-[70%] lg:max-w-[50%] 2xl:text-4xl xl:max-w-[45%]  text-primary-white'>{truncateString(movie?.overview, 170)}</p>
                 </div>
             </div>
         </div>
