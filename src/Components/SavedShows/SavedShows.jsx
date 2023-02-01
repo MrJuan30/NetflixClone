0bbc5996b4b573a9123a8c0339208adf9bf1d1e5
saac17 <saac17062003@gmail.com>
2023-02-01 00:50:35 -0500

    account firebase

--- src/Components/SavedShows/SavedShows.jsx
+++ src/Components/SavedShows/SavedShows.jsx
@@ -5,7 +5,7 @@ import { db } from '../../firebase.config'
 import { updateDoc, doc, onSnapshot } from 'firebase/firestore'
 
 const SavedShows = () => {
-    const [Movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([])
     const {user} = UserAuth()
 
     const slideLeft = () => {
@@ -18,8 +18,13 @@ const SavedShows = () => {
         slider.scrollLeft = slider.scrollLeft + 500
     }
 
+    useEffect(() => {
+        onSnapshot(doc(db, 'users', `${user?.email}`), (doc) => {
+         setMovies(doc.data()?.savedShows);
+        });
+       }, [user?.email]);
     
-    
+       console.log(movies);
 
   return (
     <>
@@ -27,7 +32,7 @@ const SavedShows = () => {
         <div className='relative flex items-center group'>
             <MdChevronLeft onClick={slideLeft} className='bg-primary-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
             <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
-                {Movies.map((item, id) => (
+                {movies?.map((item, id) => (
                     <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
                         <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.img}`} alt={item?.title} />
                         <div className='absolute flex items-center justify-center top-0 left-0 w-full h-full hover:bg-[#000]/50 opacity-0 hover:opacity-100 text-primary-white'>
